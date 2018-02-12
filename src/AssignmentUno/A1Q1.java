9f714be37a6a0b32b94845ef88c1b2cb7d303d72
pritb9521 <pritb9521@HRH-IC0027344.wrdsb.ca>
2018-02-12 11:21:11 -0500

    Question 1 complete

---
+++ src/AssignmentUno/A1Q1.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AssignmentUno;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+import java.awt.Color;
+
+/**
+ *
+ * @author pritb9521
+ */
+public class A1Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Create a city for the robot
+        City pym = new City();
+        
+        // Put a robot in lav
+        RobotSE ultron = new RobotSE(pym, 0, 2, Direction.WEST);
+        
+        // Set the robot's colour to black
+        ultron.setColor(Color.BLACK);
+        
+        // Create the walls 
+        new Wall(pym, 1, 1, Direction.WEST);
+        new Wall(pym, 2, 1, Direction.WEST);
+        new Wall(pym, 1, 1, Direction.NORTH);
+        new Wall(pym, 1, 2, Direction.NORTH);
+        new Wall(pym, 1, 2, Direction.EAST);
+        new Wall(pym, 2, 2, Direction.EAST);
+        new Wall(pym, 2, 1, Direction.SOUTH);
+        new Wall(pym, 2, 2, Direction.SOUTH);
+        
+        // ultron moves forward two spaces
+        ultron.move(2);
+        
+        ultron.turnLeft();
+        
+        // ultron moves down three spaces 
+        ultron.move(3);
+        
+        ultron.turnLeft();
+        
+        // ultron moves right three spaces
+        ultron.move(3);
+        
+        ultron.turnLeft();
+        
+        // ultron moves up three spaces
+        ultron.move(3);
+        
+        ultron.turnLeft();
+        
+        // ultron returns to the starting position
+        ultron.move();
+        
+    }
+}
