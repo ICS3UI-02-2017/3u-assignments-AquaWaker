4258ad9a527a126aff8d8933f42a168a6a47cb16
pritb9521 <pritb9521@HRH-IC0027344.wrdsb.ca>
2018-02-26 11:11:14 -0500

    Assignment 3 Finished

--- src/AssignmentTres/A3Q2.java
+++ src/AssignmentTres/A3Q2.java
@@ -11,6 +11,7 @@ import becker.robots.Thing;
 
 /**
  * Answering A3Q2
+ *
  * @author pritb9521
  */
 public class A3Q2 {
@@ -19,13 +20,13 @@ public class A3Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
         // Create a city
         City pym = new City();
-        
+
         // Create a robot in the city
         RobotSE arsenis = new RobotSE(pym, 1, 1, Direction.EAST);
-        
+
         // Create 10 Things in the same spot for arsenis to move
         new Thing(pym, 1, 1);
         new Thing(pym, 1, 1);
@@ -37,21 +38,21 @@ public class A3Q2 {
         new Thing(pym, 1, 1);
         new Thing(pym, 1, 1);
         new Thing(pym, 1, 1);
-        
+
         // Have arsenis move the Things one at a time to the spot infront of him
         for (int thingsMoved = 0; thingsMoved < 10; thingsMoved++) {
             arsenis.pickThing();
             arsenis.move();
             arsenis.pickAllThings();
-            if(arsenis.countThingsInBackpack()!= 10){
-            arsenis.putAllThings();
-            arsenis.turnAround();
-            arsenis.move();
-            arsenis.turnAround();
+            if (arsenis.countThingsInBackpack() != 10) {
+                arsenis.putAllThings();
+                arsenis.turnAround();
+                arsenis.move();
+                arsenis.turnAround();
             } else {
                 arsenis.putAllThings();
             }
         }
-        
+
     }
 }
--- src/AssignmentTres/A3Q3.java
+++ src/AssignmentTres/A3Q3.java
@@ -10,6 +10,7 @@ import becker.robots.RobotSE;
 
 /**
  * Answering A3Q3
+ *
  * @author pritb9521
  */
 public class A3Q3 {
@@ -18,23 +19,36 @@ public class A3Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // Create a city
         City pym = new City();
-        
+
         // Tell how many seeds start in the backpack
         int seedsInBackpack = 20;
-        
+
         // Create a robot in the city
         RobotSE arsenis = new RobotSE(pym, 1, 1, Direction.EAST, seedsInBackpack);
-        
-        
-        for (;seedsInBackpack <= 20;) {
-            if(arsenis.getAvenue() != 5){
+
+        // While there are still seeds in the backpack, zig zag 
+        for (; seedsInBackpack >= 1;) {
+            if (arsenis.getAvenue() < 6 && arsenis.getAvenue() > 0) {
                 arsenis.putThing();
                 seedsInBackpack = seedsInBackpack - 1;
+                arsenis.move();
+            } else if (arsenis.getAvenue() >= 6) {
+                arsenis.turnRight();
+                arsenis.move();
+            } else {
+                arsenis.turnLeft();
+                arsenis.move();
             }
-            
+
         }
+
+        // get arsenis to the spot the assignment wants him to end at
+        arsenis.turnLeft();
+        arsenis.move();
+        arsenis.turnLeft();
+        arsenis.move();
     }
 }
