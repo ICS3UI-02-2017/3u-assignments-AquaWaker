d9a5e2ee296e165a1f867362df242fd85549206a
pritb9521 <pritb9521@HRH-IC0027344.wrdsb.ca>
2018-03-19 11:18:14 -0400

    Assignmen 4 finished
    Started Ubbi Dubbi

--- src/AssignmentCinco/UbbiDubbi.java
+++ src/AssignmentCinco/UbbiDubbi.java
@@ -4,7 +4,10 @@
  */
 package AssignmentCinco;
 
+import java.util.Scanner;
+
 /**
+ * Translating Ubbi Dubbi
  *
  * @author pritb9521
  */
@@ -14,6 +17,58 @@ public class UbbiDubbi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        // Allows input
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the text to translate to Ubbi Dubbi");
+        String test = input.nextLine();
+
+        // sanitize the input
+        test = test.toLowerCase();
+
+        //get the length of the string
+        int length = test.length();
+        
+        // Checks for vowels and adds ub in front of them
+        for (int vowelCheck = 0; vowelCheck <= length; vowelCheck++) {
+
+            if (test.charAt(vowelCheck) == 'u') {
+                if (test.charAt(vowelCheck + 1) != 'e' && test.charAt(vowelCheck + 1) != 'a' && test.charAt(vowelCheck + 1) != 'i' && test.charAt(vowelCheck + 1) != 'o' && test.charAt(vowelCheck + 1) != 'u') {
+                    test = test.replace("u", "ubu");
+                    length = test.length();
+                    vowelCheck = vowelCheck + 2;
+                }
+            } else if (test.charAt(vowelCheck) == 'e') {
+                if (test.charAt(vowelCheck + 1) != 'e' && test.charAt(vowelCheck + 1) != 'a' && test.charAt(vowelCheck + 1) != 'i' && test.charAt(vowelCheck + 1) != 'o' && test.charAt(vowelCheck + 1) != 'u') {
+                    test = test.replace("e", "ube");
+                    length = test.length();
+                    vowelCheck = vowelCheck + 2;
+                }
+            } else if (test.charAt(vowelCheck) == 'a') {
+                if (test.charAt(vowelCheck + 1) != 'e' && test.charAt(vowelCheck + 1) != 'a' && test.charAt(vowelCheck + 1) != 'i' && test.charAt(vowelCheck + 1) != 'o' && test.charAt(vowelCheck + 1) != 'u') {
+                    test = test.replace("a", "uba");
+                    length = test.length();
+                    vowelCheck = vowelCheck + 2;
+                }
+            } else if (test.charAt(vowelCheck) == 'i') {
+                if (test.charAt(vowelCheck + 1) != 'e' && test.charAt(vowelCheck + 1) != 'a' && test.charAt(vowelCheck + 1) != 'i' && test.charAt(vowelCheck + 1) != 'o' && test.charAt(vowelCheck + 1) != 'u') {
+                    test = test.replace("i", "ubi");
+                    length = test.length();
+                    vowelCheck = vowelCheck + 2;
+                }
+            } else if (test.charAt(vowelCheck) == 'o') {
+                if (test.charAt(vowelCheck + 1) != 'e' && test.charAt(vowelCheck + 1) != 'a' && test.charAt(vowelCheck + 1) != 'i' && test.charAt(vowelCheck + 1) != 'o' && test.charAt(vowelCheck + 1) != 'u') {
+                    test = test.replace("o", "ubo");
+                    length = test.length();
+                    vowelCheck = vowelCheck + 2;
+                }
+
+            }
+
+        }
+
+        System.out.print("Your translation into Ubbi Dubbi is: ");
+        System.out.println(test);
     }
 }
