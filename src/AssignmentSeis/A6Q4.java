7719b594f8af4d6f429530140d6b49a192e548b6
pritb9521 <pritb9521@HRH-IC0027344.wrdsb.ca>
2018-04-04 11:17:31 -0400

    Started Assignment 6
    Finished A6 Q1,2 and 3.  Started A6Q4

---
+++ src/AssignmentSeis/A6Q4.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AssignmentSeis;
+
+/**
+ * Answering A6Q4
+ * @author pritb9521
+ */
+public class A6Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+}
