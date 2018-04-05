d4f338ecc31fca31b77be04287da8c8953105554
pritb9521 <pritb9521@HRH-IC0027344.wrdsb.ca>
2018-04-05 11:19:02 -0400

    Finished A6 Q1, 2 and 3.  Started A6Q4
    Finished A6 Q4, 5. Started A6Q4

---
+++ src/AssignmentSeis/A6Q6.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AssignmentSeis;
+
+import java.util.Scanner;
+
+/**
+ * Answering A6Q6
+ *
+ * @author pritb9521
+ */
+public class A6Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Allows input
+        Scanner input = new Scanner(System.in);
+
+        // Gain knowledge of the amount of marks
+        System.out.print("How many students are there? : ");
+        int handedIn = input.nextInt();
+
+        // Gain thyne knowledge of all the marks
+        System.out.println("Please enter the " + handedIn + " marks: ");
+        double[] integers = new double[handedIn];
+        for (int tInt = 0; tInt < handedIn; tInt++) {
+            integers[tInt] = input.nextInt();
+        }
+
+        // If the first mark is larger, swap their positon, and repeat for all the marks
+        for (int swapP2 = 0; swapP2 < (handedIn - 1); swapP2++) {
+            for (int swapP1 = 0; swapP1 < handedIn; swapP1++) {
+                double swap = 0;
+                if (swapP1 != 0) {
+                    if (integers[swapP1 - 1] > integers[swapP1]) {
+                        swap = integers[swapP1 - 1];
+                        integers[swapP1 - 1] = integers[swapP1];
+                        integers[swapP1] = swap;
+                    }
+                }
+            }
+        }
+        
+        
+        // State the highest, the lowest, and the average mark
+        System.out.println("The highest mark is " + integers[integers.length - 1] + "% /n" 
+                + "The highest mark is " + integers[0] + "% /n"
+                + "The average mark is ");
+    }
+}
+
